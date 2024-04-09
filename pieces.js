5d93f43407913893e48d5ffbf423b7ab8e8d67cd
landolsi-yanis <landolsiyanis@gmail.com>
2024-04-10 00:10:53 +0200

    Added filter and sort buttons

--- pieces.js
+++ pieces.js
@@ -1,49 +1,57 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
-
-
-
-
 
 for (let i = 0; i < pieces.length; i++) {
 
+    const article = pieces[i];
     // Récupération de l'élément du DOM qui accueillera les fiches
     const sectionFiches = document.querySelector(".fiches");
     // Création d’une balise dédiée à une pièce automobile
     const pieceElement = document.createElement("article");
-    // On crée l’élément img.
+    // Création des balises 
     const imageElement = document.createElement("img");
-    // On accède à l’indice i de la liste pieces pour configurer la source de l’image.
-    imageElement.src = pieces[i].image;
-    // Idem pour le nom, le prix et la catégorie...
-
+    imageElement.src = article.image;
     const nomElement = document.createElement("h2");
-    nomElement.innerText = pieces[i].nom;
-
+    nomElement.innerText = article.nom;
     const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€" : "€€€"})`;
-
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
     const categorieElement = document.createElement("p");
-    categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
-
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
     const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment";
-
-    const disponibiliteElement = document.createElement("p");
-    disponibiliteElement.innerText = pieces[i].disponibilite ? "En stock" : "Rupture de stock";
-
-    // On rattache la balise article à la section Fiches
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
     // On rattache l’image à pieceElement (la balise article)
     pieceElement.appendChild(imageElement);
     pieceElement.appendChild(nomElement);
-    // Idem pour le nom, le prix et la catégorie...
-    pieceElement.appendChild(nomElement);
     pieceElement.appendChild(prixElement);
     pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
     pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(disponibiliteElement);
+    pieceElement.appendChild(stockElement);
 
-    }
+ }
+ 
+ //gestion des boutons 
+const boutonTrier = document.querySelector(".btn-trier");
 
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+     });
+     console.log(piecesOrdonnees);
+});
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+   console.log(piecesFiltrees)
+});
